fd25b1a2b724e7c2ef2cb356cf97fca02634304c
Vlados <malak3@mail.ru>
2026-04-23 05:38:31 +0300

    add seeders- product,category

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'price' => fake()->randomFloat(2, 0, 100),
+            'category_id' => fake()->numberBetween(1, 10),
+        ];
+    }
+}
